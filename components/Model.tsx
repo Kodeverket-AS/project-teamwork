b8d4752f79600a09cc9607d03eb38e2ab169cf72
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-10-24 13:24:41 +0200

    setting up model section

--- components/Model.tsx
+++ components/Model.tsx
@@ -2,11 +2,80 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import ModelImage from "../public/ModelImage.jpg";
+import Icon1 from "../public/icons/bxl_graphql.svg";
+import Icon2 from "../public/icons/octicon_graph-24.svg";
+import Icon3 from "../public/icons/material-symbols-light_handshake-outline.svg";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 export default function Model() {
   return (
-    <div className="bg-teamwork-secondary-orange h-36 w-full">
-      Here comes info about the model
+    <div className="bg-teamwork-secondary-orange w-full flex flex-col md:flex-row px-5 md:px-10 py-14">
+      <div className="flex flex-col gap-3 w-full md:w-1/2">
+        <h2>Modell</h2>
+        <div className="bg-kv-white text-kv-black shadow px-3 py-2">
+          <div className="flex flex-row">
+            <Image
+              src={Icon1}
+              width={100}
+              height={100}
+              className="w-auto h-10"
+              alt="ikon tilhørende et ledende fagmiljø"
+            />
+            <h3>Et ledende fagmiljø</h3>
+          </div>
+          <p>
+            Here comes a text about this subject, please change this before the
+            page goes live it's critical and btw I need to fill the space all
+            the time you have to leave the space. Just leave me alone, I know
+            what to do.
+          </p>
+        </div>
+        <div className="bg-kv-white text-kv-black shadow px-3 py-2">
+          <div className="flex flex-row">
+            <Image
+              src={Icon2}
+              width={100}
+              height={100}
+              className="w-auto h-10"
+              alt="ikon tilhørende et ledende fagmiljø"
+            />
+            <h3>Medarbeiderskap</h3>
+          </div>
+          <p>
+            Medarbeiderskap er Nordens ideal for samspill og ansvarsdeling på
+            arbeidsplassen. Sammen med professor Stefan Tengblad i Göteborg, har
+            Johan Velten bidratt til å gi begrepet et konkret meningsinnhold og
+            verktøy for praktisk utvikling.
+          </p>
+        </div>
+        <div className="bg-kv-white text-kv-black shadow px-3 py-2">
+          <div className="flex flex-row">
+            <Image
+              src={Icon3}
+              width={100}
+              height={100}
+              className="w-auto h-10"
+              alt="ikon tilhørende et ledende fagmiljø"
+            />
+            <h3>Solid samarbeidspartner</h3>
+          </div>
+          <p>
+            Here comes a text about this subject, please change this before the
+            page goes live it's critical and btw I need to fill the space all
+            the time you have to leave the space. Just leave me alone, I know
+            what to do.
+          </p>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2">
+        <Image
+          src={ModelImage}
+          width={1000}
+          height={750}
+          alt="Mann i dressjakke som ser mot kamera"
+        />
+      </div>
     </div>
   );
 }
