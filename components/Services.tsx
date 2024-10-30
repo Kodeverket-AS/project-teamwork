9baf5c2af84d451bfa2038cf55c1f479dc4d4608
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-10-30 15:30:18 +0100

    services skeleton

--- components/Services.tsx
+++ components/Services.tsx
@@ -5,8 +5,30 @@ import Image from "next/image";
 
 export default function Services() {
   return (
-    <div className="bg-kv-white h-36 w-full">
-      Here comes the services section
+    <div className="bg-kv-white w-full flex flex-col">
+      <h2>Tjenester</h2>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-28 h-36 shadow">
+          <h3>Medarbeiderutvikling</h3>
+        </div>
+        <div className="w-28 h-36 shadow">
+          <h3>Teamutvikling</h3>
+        </div>
+        <div className="w-28 h-36 shadow">
+          <h3>Lederutvikling</h3>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-28 h-36 shadow">
+          <h3>Omstilling og nedbemanning</h3>
+        </div>
+        <div className="w-28 h-36 shadow">
+          <h3>Konfliktløsing</h3>
+        </div>
+        <div className="w-28 h-36 shadow">
+          <h3>Coaching</h3>
+        </div>
+      </div>
     </div>
   );
 }
