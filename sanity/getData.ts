4e41edbc8dbe8718ba7bed62d92139b421af6391
kristoffer <kunkristoffer@gmail.com>
2024-11-07 18:56:46 +0100

    Added missing groq queries

--- sanity/getData.ts
+++ sanity/getData.ts
@@ -2,7 +2,10 @@ import { Client } from "./sanity";
 
 export async function getData() {
   return Client.fetch(`{
-    "services": *[_type == "services"] {title, content, "image":image.asset->url, "alt":image.alt, _id},
+    "books": *[_type == 'books'] { ..., "image":image.asset->url},
+    "customers": *[_type == 'customers'] { ..., "image": {"url": image.asset->url }},
+    "feedback": *[_type == 'feedback'] { ..., "image": {"url": image.asset->url }},
+    "services": *[_type == 'services'] { ..., "image":image.asset->url, "alt":image.alt, _id},
+    "team": *[_type == 'team'] { ..., "image": {"url": image.asset->url }},
   }`);
-  //{ cache: "no-cache" }
 }
