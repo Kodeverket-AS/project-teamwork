71f84bcf16ec1b27dddfd3c0b9be87b85d3527b5
kristoffer <kunkristoffer@gmail.com>
2024-11-07 18:11:38 +0100

    Rewrote services to use context provider instead of props

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,92 +1,30 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
+import { useSanityContext } from "@/context/sanity";
 
-interface Contents {
-  title: string;
-  content: string;
-  image: string;
-  alt: string;
-}
+export default function Services() {
+  const { services } = useSanityContext()
 
-interface ServicesProps {
-  content: Contents[];
-}
-
-export default function Services({ content }:ServicesProps) {
   return (
     <div className="bg-kv-white w-full flex flex-col px-5 md:px-10 py-14 gap-10">
       <h2>Tjenester</h2>
       <div className="flex flex-col md:flex-row w-full md:justify-between gap-10">
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[0].image}
-            width={400}
-            height={400}
-            alt={content[0].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[0].title}</h3>
-          <p>{content[0].content}</p>
-        </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[1].image}
-            width={400}
-            height={400}
-            alt={content[1].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[1].title}</h3>
-          <p>{content[1].content}</p>
-        </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[2].image}
-            width={400}
-            height={400}
-            alt={content[2].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[2].title}</h3>
-          <p>{content[2].content}</p>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row w-full md:justify-between gap-10">
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[3].image}
-            width={400}
-            height={400}
-            alt={content[3].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[3].title}</h3>
-          <p>{content[3].content}</p>
-        </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[4].image}
-            width={400}
-            height={400}
-            alt={content[4].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[4].title}</h3>
-          <p>{content[4].content}</p>
-        </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[5].image}
-            width={400}
-            height={400}
-            alt={content[5].alt}
-            className="block group-hover:hidden"
-          />
-          <h3>{content[5].title}</h3>
-          <p>{content[0].content}</p>
-        </div>
+        {
+          services && services.map((item) => (
+            <div key={item._id} className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
+              <Image
+                src={item.image?.url ? item.image.url : ""}
+                width={400}
+                height={400}
+                alt={item.image?.alt ? item.image.alt : "Something went wrong loading the images"}
+                className="block group-hover:hidden"
+              />
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
