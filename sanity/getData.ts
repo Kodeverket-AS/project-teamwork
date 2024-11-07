6a63d9903cf216f9f725d88477bb146c0b7d7f6b
kristoffer <kunkristoffer@gmail.com>
2024-11-07 17:03:49 +0100

    Removed old sanity data fetch methods

--- sanity/getData.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "./sanity";
-
-export async function getData() {
-  return Client.fetch(`{
-    "services": *[_type == "services"] {title, content, "image":image.asset->url, "alt":image.alt, _id},
-  }`);
-  //{ cache: "no-cache" }
-}
