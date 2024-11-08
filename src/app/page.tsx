baabecbc6c74d6195ed7ec3bdbd3e62d8cdb85a4
Kristoffer <kristofferfh.kodehode@gmail.com>
2024-11-08 14:21:28 +0100

    Renamed type import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Hero from "@/components/Hero";
 import Model from "@/components/Model";
 import Services from "@/components/Services";
@@ -5,17 +6,20 @@ import Books from "@/components/Books";
 import Histories from "@/components/Histories";
 import TeamMembers from "@/components/Team";
 import Success from "@/components/Success";
+import { useSanityContext } from "@/context/sanity";
 
 export default async function Home() {
+  const { services, books, team, feedback } = useSanityContext()
+
   return (
     <main className="flex flex-col items-center justify-between">
       <Hero />
       <Model />
-      <Services />
+      <Services content={services}/>
       <Histories />
-      <Books />
-      <TeamMembers />
-      <Success />
+      <Books content={books}/>
+      <TeamMembers content={team}/>
+      <Success content={feedback}/>
     </main>
   );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Services as tServices } from "@/types/sanity.types";
 import SectionComponent from "./sections/SectionComponent";
 
 interface Contents {
@@ -11,7 +12,7 @@ interface Contents {
 export default function Services({
   content,
 }: {
-  content: Contents[];
+  content: tServices[];
 }) {
   return (
     <SectionComponent orange={false}>
