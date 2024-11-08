79ff2f1134eb323bd41b8914e406b03275853bb6
Kristoffer <kristofferfh.kodehode@gmail.com>
2024-11-08 10:11:29 +0100

    Removing old fetch function and prop passing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,16 @@ import Books from "../../components/Books";
 import Histories from "../../components/Histories";
 import Team from "../../components/Team";
 import Success from "../../components/Success";
-import { getData } from "../../sanity/getData";
-import { dummyData } from "../../components/dummydata/dummydata";
 
 export default async function Home() {
-  const data = await getData();
-  // console.log(data);
-  const services = data.services;
-  const team = dummyData.teamData; // dummydata. Replace with data when ready
-  const books = dummyData.booksData; // dummydata. Replace with data when ready
-
   return (
     <main className="flex flex-col items-center justify-between">
       <Hero />
       <Model />
       <Services />
       <Histories />
-      <Books content={books} />
-      <Team content={team} />
+      <Books />
+      <Team />
       <Success />
     </main>
   );
