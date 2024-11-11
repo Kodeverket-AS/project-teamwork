3ba875daf4a9afad3c423902f910efb1a90d39b6
kristoffer <kunkristoffer@gmail.com>
2024-11-11 10:31:58 +0100

    Changed component to use .map render instead of static arr index

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,43 +3,24 @@
 import { Services as tServices } from "@/types/sanity.types";
 import SectionComponent from "./sections/SectionComponent";
 
-interface Contents {
-  title: string;
-  content: string;
-  image: string;
-}
-
-export default function Services({
-  content,
-}: {
-  content: tServices[];
-}) {
+export default function Services({ content }: { content: tServices[] }) {
   return (
     <SectionComponent orange={false}>
       <div className="flex flex-col gap-10">
         <h2>Tjenester</h2>
-        <div className="flex flex-col md:flex-row w-full md:justify-between gap-10">
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[0].title}</h3>
+        {content && content.map(item => (
+          <div key={item._id} className="flex flex-col md:flex-row w-full md:justify-between gap-10">
+            <div className="w-full h-60 shadow md:w-56 md:h-64">
+              <h3>{item.title}</h3>
+            </div>
+            <div className="w-full h-60 shadow md:w-56 md:h-64">
+              <h3>{item.title}</h3>
+            </div>
+            <div className="w-full h-60 shadow md:w-56 md:h-64">
+              <h3>{item.title}</h3>
+            </div>
           </div>
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[1].title}</h3>
-          </div>
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[2].title}</h3>
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row w-full md:justify-between gap-10">
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[3].title}</h3>
-          </div>
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[4].title}</h3>
-          </div>
-          <div className="w-full h-60 shadow md:w-56 md:h-64">
-            <h3>{content[5].title}</h3>
-          </div>
-        </div>
+        ))}
       </div>
     </SectionComponent>
   );
