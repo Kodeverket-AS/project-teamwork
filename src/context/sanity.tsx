9d155129d585e52c8e0c23514e17f6672e596510
Kristian <kristian007.kodehode@gmail.com>
2024-11-12 12:12:06 +0100

    updated some files from dev branch

--- src/context/sanity.tsx
+++ src/context/sanity.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { createContext, ReactNode, useContext } from "react";
-import { SanityData } from "../types/sanity.types";
-import { useSanity } from "../hooks/useSanityHooks";
+import { SanityData } from "@/types/sanity.types";
+import { useSanity } from "@/hooks/useSanityHooks";
 
-// Initiate context
-const SanityContext = createContext<SanityData>(null);
+// Initiate a context
+const SanityContext = createContext<SanityData>({
+  books: [],
+  customers: [],
+  feedback: [],
+  services: [],
+  team: [],
+});
 
-// export context consumer
+/**
+ * Hook for using SanityData, you can destructure return object to use specific fields.
+ * While arrays might be empty, they are always defined.
+ * @returns {SanityData}
+ */
 export const useSanityContext = () => {
   const sanity: SanityData = useContext(SanityContext);
   return { ...sanity };
