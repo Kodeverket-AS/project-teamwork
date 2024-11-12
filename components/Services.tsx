bbb4f7cff74ec6e1c85c1f12ddaa6697c677d924
Kristian <kristian007.kodehode@gmail.com>
2024-11-12 12:39:39 +0100

    services update

--- components/Services.tsx
+++ components/Services.tsx
@@ -11,11 +11,6 @@ interface Contents {
 
 interface ServicesProps {
   content: Contents[];
-  alt: string;
-}
-
-interface ServicesProps {
-  content: Contents[];
 }
 
 export default function Services({ content }: ServicesProps) {
@@ -31,26 +26,9 @@ export default function Services({ content }: ServicesProps) {
             alt={content[0].alt}
             className="block group-hover:hidden"
           />
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[0].image}
-            width={400}
-            height={400}
-            alt={content[0].alt}
-            className="block group-hover:hidden"
-          />
           <h3>{content[0].title}</h3>
           <p>{content[0].content}</p>
-          <p>{content[0].content}</p>
         </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[1].image}
-            width={400}
-            height={400}
-            alt={content[1].alt}
-            className="block group-hover:hidden"
-          />
         <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
           <Image
             src={content[1].image}
@@ -61,7 +39,6 @@ export default function Services({ content }: ServicesProps) {
           />
           <h3>{content[1].title}</h3>
           <p>{content[1].content}</p>
-          <p>{content[1].content}</p>
         </div>
         <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
           <Image
@@ -71,16 +48,7 @@ export default function Services({ content }: ServicesProps) {
             alt={content[2].alt}
             className="block group-hover:hidden"
           />
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[2].image}
-            width={400}
-            height={400}
-            alt={content[2].alt}
-            className="block group-hover:hidden"
-          />
           <h3>{content[2].title}</h3>
-          <p>{content[2].content}</p>
           <p>{content[2].content}</p>
         </div>
       </div>
@@ -93,26 +61,9 @@ export default function Services({ content }: ServicesProps) {
             alt={content[3].alt}
             className="block group-hover:hidden"
           />
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[3].image}
-            width={400}
-            height={400}
-            alt={content[3].alt}
-            className="block group-hover:hidden"
-          />
           <h3>{content[3].title}</h3>
           <p>{content[3].content}</p>
-          <p>{content[3].content}</p>
         </div>
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[4].image}
-            width={400}
-            height={400}
-            alt={content[4].alt}
-            className="block group-hover:hidden"
-          />
         <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
           <Image
             src={content[4].image}
@@ -123,7 +74,6 @@ export default function Services({ content }: ServicesProps) {
           />
           <h3>{content[4].title}</h3>
           <p>{content[4].content}</p>
-          <p>{content[4].content}</p>
         </div>
         <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
           <Image
@@ -133,16 +83,7 @@ export default function Services({ content }: ServicesProps) {
             alt={content[5].alt}
             className="block group-hover:hidden"
           />
-        <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
-          <Image
-            src={content[5].image}
-            width={400}
-            height={400}
-            alt={content[5].alt}
-            className="block group-hover:hidden"
-          />
           <h3>{content[5].title}</h3>
-          <p>{content[0].content}</p>
           <p>{content[0].content}</p>
         </div>
       </div>
