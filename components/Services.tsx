bd21b0b8f3d28da33b678704bf0f6718c1238cee
kristoffer <kunkristoffer@gmail.com>
2024-11-12 17:08:02 +0100

    Added missing key in arr map. Added css grid-col breakpoints

--- components/Services.tsx
+++ components/Services.tsx
@@ -7,9 +7,9 @@ export default function Services({ content }: { content: tServices[]}) {
   return (
     <div className="bg-kv-white w-full flex flex-col px-5 md:px-10 py-14 gap-10">
       <h2>Tjenester</h2>
-      <div className="grid grid-cols-3 w-full gap-10">
+      <div className="grid grid-cols-3 max-sm:grid-cols-1 max-lg:grid-cols-2 w-full gap-10">
         {content.map(item => (
-          <div className="w-full h-60 shadow md:w-1/3 md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
+          <div key={item._id} className="h-60 shadow md:h-72 overflow-hidden p-3 hover:border-teamwork-primary-orange hover:border group">
             <Image
               src={item.image?.url ?? 'HeroImage.jpg'}
               width={400}
@@ -21,7 +21,7 @@ export default function Services({ content }: { content: tServices[]}) {
             <p>{item.content}</p>
           </div>
         ))}
-      </div> 
+      </div>
     </div>
   );
 }
