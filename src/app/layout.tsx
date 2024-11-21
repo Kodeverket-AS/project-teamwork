46e98f642b7655789ff51681bd982fb1053b0f69
kristoffer <kunkristoffer@gmail.com>
2024-11-21 10:36:14 +0100

    Moving cononical into metadata ref: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,91 +1,89 @@
 import type { Metadata } from "next";
 import { Inter, Ysabeau_SC } from "next/font/google";
-import "./globals.css";
 import { ScrollToTopButton } from "../../components/Buttons";
 import { NavbarAlt } from "../../components/navigation/NavBar";
-import Footer from "../../components/Footer";
 import { SanityProvider } from "@/context/sanity";
+import Footer from "../../components/Footer";
+import "./globals.css";
 
 const inter = Inter({
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 const ysabeauSC = Ysabeau_SC({
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-heading",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
 });
 
 export const metadata: Metadata = {
-    title: {
-        default: "Teamwork",
-        template: "%s | Teamwork",
+  title: {
+    default: "Teamwork",
+    template: "%s | Teamwork",
+  },
+  description: "Frigjør kraften i medarbeiderskapet",
+  keywords: [
+    "medarbeiderskap",
+    "skandinaviske ledelsesmodellen",
+    "employeeship",
+  ],
+  authors: [{ name: "Teamwork" }],
+  creator: "Teamwork",
+  publisher: "Teamwork",
+  metadataBase: new URL("https://www.teamwork.no"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
+  },
+  openGraph: {
+    type: "website",
+    locale: "nb_NO",
+    url: "/",
+    title: "Teamwork",
     description: "Frigjør kraften i medarbeiderskapet",
-    keywords: [
-        "medarbeiderskap",
-        "skandinaviske ledelsesmodellen",
-        "employeeship",
+    siteName: "teamwork.no",
+    images: [
+      {
+        url: "/LogoLight.svg",
+        width: 1200,
+        height: 630,
+        alt: "Teamwork Logo",
+      },
     ],
-    authors: [{ name: "Teamwork" }],
-    creator: "Teamwork",
-    publisher: "Teamwork",
-    metadataBase: new URL("https://www.teamwork.no"),
-    robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-        },
-    },
-    openGraph: {
-        type: "website",
-        locale: "nb_NO",
-        url: "/",
-        title: "Teamwork",
-        description: "Frigjør kraften i medarbeiderskapet",
-        siteName: "teamwork.no",
-        images: [
-            {
-                url: "/LogoLight.svg",
-                width: 1200,
-                height: 630,
-                alt: "Teamwork Logo",
-            },
-        ],
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: "Teamwork",
-        description: "Beskrivelse for Twitter",
-        images: ["/LogoLight.svg"],
-    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Teamwork",
+    description: "Beskrivelse for Twitter",
+    images: ["/LogoLight.svg"],
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='nb' className={`${inter.variable} ${ysabeauSC.variable}`}>
-            <head>
-                <link rel='canonical' href='https://teamwork.no' />
-            </head>
-            <body
-                className={`${inter.className} antialiased overflow-x-hidden`}
-            >
-                <NavbarAlt />
-                <SanityProvider>{children}</SanityProvider>
-                <ScrollToTopButton />
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="nb" className={`${inter.variable} ${ysabeauSC.variable}`}>
+      <body className={`${inter.className} antialiased overflow-x-hidden`}>
+        <NavbarAlt />
+        <SanityProvider>{children}</SanityProvider>
+        <ScrollToTopButton />
+        <Footer />
+      </body>
+    </html>
+  );
 }
