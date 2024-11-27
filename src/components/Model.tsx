76099bca0822b0f86c01f59e66b0ba821c9812ca
Kristian <kristian007.kodehode@gmail.com>
2024-11-27 13:24:39 +0100

    added animation to accordion. minor fixes. replaced image in accordion with icons imported with react icons. made one reusable component for the accordion item. mapping. soon done.

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,115 +1,120 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
+import { MdOutlineHandshake } from "react-icons/md";
+import { MdJoinLeft } from "react-icons/md";
+
 import SectionComponent from "./sections/SectionComponent";
 
-export default function Model() {
-  const [show1, setShow1] = useState(false);
-  const [show2, setShow2] = useState(false);
-  const [show3, setShow3] = useState(false);
+type TAccordionProps = {
+  subjectIcon: React.ReactElement;
+  title: string;
+  content: string;
+  isOpen?: boolean;
+  toggle?: () => void;
+};
+
+const accordionData: TAccordionProps[] = [
+  {
+    subjectIcon: <RiTeamFill />,
+    title: "Ledende fagmiljø",
+    content:
+      "Here comes a text about this subject, please change this before the page goes live it's critical and btw I need to fill the space all the time you have to leave the space. Just leave me alone, I know what to do.",
+  },
+  {
+    subjectIcon: <MdJoinLeft />,
+    title: "Medarbeiderskap",
+    content:
+      "Medarbeiderskap er Nordens ideal for samspill og ansvarsdeling på arbeidsplassen. Sammen med professor Stefan Tengblad i Göteborg, har Johan Velten bidratt til å gi begrepet et konkret meningsinnhold og verktøy for praktisk utvikling.",
+  },
+  {
+    subjectIcon: <MdOutlineHandshake />,
+    title: "Solid samarbeidspartner",
+    content:
+      "Here comes a text about this subject, please change this before the page goes live it's critical and btw I need to fill the space all the time you have to leave the space. Just leave me alone, I know what to do.",
+  },
+];
+
+const AccordionComponent: React.FC<TAccordionProps> = ({
+  subjectIcon,
+  title,
+  content,
+  isOpen,
+  toggle,
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState("0px");
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(
+        isOpen ? `${contentRef.current.scrollHeight}px` : "0px"
+      );
+    }
+  }, [isOpen]);
+
   return (
-    <SectionComponent orange={true} fade>
+    <div className="bg-kv-white text-kv-black shadow px-3 transition-all duration-500 ease-in-out border-b">
+      <button
+        onClick={toggle}
+        className="group w-full py-4">
+        <div
+          className={`text-xl flex flex-row justify-between items-center `}>
+          <div className="flex flex-row items-center gap-2">
+            <div
+              className={`${isOpen ? "text-teamwork-primary-orange" : "text-kv-black"}`}>
+              {subjectIcon}
+            </div>
+            <h4 className="no-underline group-hover:underline-offset-4 group-hover:underline">
+              {title}
+            </h4>
+          </div>
+          <div className="animate-none group-hover:animate-pulse">
+            {isOpen ? <FaArrowUp /> : <FaArrowDown />}
+          </div>
+        </div>
+      </button>
+      <div
+        ref={contentRef}
+        style={{ maxHeight: height }}
+        className="transition-max-height duration-500 ease-in-out overflow-hidden">
+        <div className={`pb-8 `}>
+          <p>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function Model() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const toggleAccordion = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+  return (
+    <SectionComponent
+      orange={true}
+      fade>
       <div
         id="model"
-        className="w-full flex flex-col md:flex-row py-14 gap-10"
-      >
-        <div className="flex flex-col gap-3 w-full md:w-1/2">
+        className="w-full flex flex-col md:flex-row py-14 gap-10">
+        <div className="flex flex-col  w-full md:w-1/2">
           <h2>Modell</h2>
-          <div
-            className={`bg-kv-white text-kv-black shadow px-3 py-2 ${
-              show1 ? "h-fit" : "h-14 overflow-hidden"
-            }`}
-          >
-            <button onClick={() => setShow1(!show1)} className="w-full">
-              <div className="flex flex-row justify-between">
-                <div className="flex flex-row justify-center items-center">
-                  <Image
-                    src="/icons/bxl_graphql.svg"
-                    width={100}
-                    height={100}
-                    className="w-auto h-10"
-                    alt="ikon tilhørende et ledende fagmiljø"
-                  />
-                  <h4>Et ledende fagmiljø</h4>
-                </div>
-                {show1 ? (
-                  <FaArrowUp className="w-6 h-auto" />
-                ) : (
-                  <FaArrowDown className="w-6 h-auto" />
-                )}
-              </div>
-            </button>
-            <p>
-              Here comes a text about this subject, please change this before
-              the page goes live it's critical and btw I need to fill the space
-              all the time you have to leave the space. Just leave me alone, I
-              know what to do.
-            </p>
-          </div>
-          <div
-            className={`bg-kv-white text-kv-black shadow px-3 py-2 ${
-              show2 ? "h-fit" : "h-14 overflow-hidden"
-            }`}
-          >
-            <button onClick={() => setShow2(!show2)} className="w-full">
-              <div className="flex flex-row justify-between">
-                <div className="flex flex-row justify-center items-center">
-                  <Image
-                    src="/icons/octicon_graph-24.svg"
-                    width={100}
-                    height={100}
-                    className="w-auto h-10"
-                    alt="ikon tilhørende et ledende fagmiljø"
-                  />
-                  <h4>Medarbeiderskap</h4>
-                </div>
-                {show2 ? (
-                  <FaArrowUp className="w-6 h-auto" />
-                ) : (
-                  <FaArrowDown className="w-6 h-auto" />
-                )}
-              </div>
-            </button>
-            <p>
-              Medarbeiderskap er Nordens ideal for samspill og ansvarsdeling på
-              arbeidsplassen. Sammen med professor Stefan Tengblad i Göteborg,
-              har Johan Velten bidratt til å gi begrepet et konkret
-              meningsinnhold og verktøy for praktisk utvikling.
-            </p>
-          </div>
-          <div
-            className={`bg-kv-white text-kv-black shadow px-3 py-2 ${
-              show3 ? "h-fit" : "h-14 overflow-hidden"
-            }`}
-          >
-            <button onClick={() => setShow3(!show3)} className="w-full">
-              <div className="flex flex-row justify-between">
-                <div className="flex flex-row justify-center items-center">
-                  <Image
-                    src="/icons/material-symbols-light_handshake-outline.svg"
-                    width={100}
-                    height={100}
-                    className="w-auto h-10"
-                    alt="ikon tilhørende et ledende fagmiljø"
-                  />
-                  <h4>Solid samarbeidspartner</h4>
-                </div>
-                {show3 ? (
-                  <FaArrowUp className="w-6 h-auto" />
-                ) : (
-                  <FaArrowDown className="w-6 h-auto" />
-                )}
-              </div>
-            </button>
-            <p>
-              Here comes a text about this subject, please change this before
-              the page goes live it's critical and btw I need to fill the space
-              all the time you have to leave the space. Just leave me alone, I
-              know what to do.
-            </p>
-          </div>
+
+          {accordionData.map((section, index) => (
+            <AccordionComponent
+              key={index}
+              subjectIcon={section.subjectIcon}
+              title={section.title}
+              content={section.content}
+              isOpen={openIndex === index}
+              toggle={() => toggleAccordion(index)}
+            />
+          ))}
         </div>
         <div className="w-full md:w-1/2">
           <Image
