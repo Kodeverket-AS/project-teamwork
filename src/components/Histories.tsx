c6cde8cf51e1afd80ed4e8750c257acd41b76440
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-12-06 10:46:09 +0100

    fixed history overflow

--- src/components/Histories.tsx
+++ src/components/Histories.tsx
@@ -35,8 +35,8 @@ export default function Histories() {
       fade>
       <div id="histories">
         <h2 className="pb-10">Historier</h2>
-        <div className="flex flex-col sm:h-[528px] sm:flex-row gap-4 sm:gap-10 justify-between items-center">
-          <div className="w-full h-64 sm:h-full rounded-lg overflow-hidden">
+        <div className="flex flex-col sm:h-[528px] lg:flex-row gap-4 sm:gap-10 justify-between items-center">
+          <div className="w-full h-64 lg:h-full rounded-lg overflow-hidden">
             {dummyData.image ? (
               <Image
                 src={dummyData.image}
